1f0f79c6f4e2404cda3b00676f7908a00f9d1c56
TangZichen0102 <110572306@qq.com>
2022-07-07 12:17:56 +0800

    save

---
+++ C++  程序设计/P1011.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int a, b, c, d;
+int main() {
+	cin >> a >>b >> c >>d;
+	cout << a *0.1 +b * 0.2 +c * 0.2 + d*0.5; 
+	return 0;
+}
---
+++ C++  程序设计/P1012.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n;
+int main() {
+    cin >> n;
+    if(n % 2 == 1) cout << "odd";
+    return 0;
+}
