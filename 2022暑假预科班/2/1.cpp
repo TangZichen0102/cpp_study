82b7fa9e395f91c2e8abbf21822b7ec8e795808a
TT <tt-tjw@qq.com>
2022-07-08 12:12:28 +0800

    save

--- 2022暑假预科班/2/1.cpp
+++ 2022暑假预科班/2/1.cpp
@@ -1,89 +1,75 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-char c[3][3];
+
 int sum = 0, sum2 = 0;
-void fun1() {
-    if(c[0][0] == c[0][1] && c[0][1] == c[0][2]) sum++;
-    if(c[1][0] == c[1][1] && c[1][1] == c[1][2]) sum++;
-    if(c[2][0] == c[2][1] && c[2][1] == c[2][2]) sum++;
+
+char a[9];
+int dir[8][3] = {
+    {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+
+int s[26];
+int d[26][26];
+
+void fun()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        set<int> dp;
+        for (int j = 0; j < 3; j++)
+            dp.insert(a[dir[i][j]] - 'A');
+
+        // for (auto ddd : dp)
+        // {
+        //     cout << ddd << " ";
+        // }
+        // cout << endl;
+
+        if (dp.size() == 1)
+        {
+            s[*dp.begin()] = 1;
+        }
+
+        if (dp.size() == 2)
+        {
+            vector<int> _dp(dp.begin(),dp.end());
+
+            // _dp.assign(dp.begin(),dp.end());
+
+            d[_dp[0]][_dp[1]] = 1;
+        }
+    }
 }
 
-void fun2() {
-    if(c[0][0] == c[1][0] && c[1][0] == c[2][0]) sum++;
-    if(c[0][1] == c[1][1] && c[1][1] == c[2][1]) sum++;
-    if(c[0][2] == c[1][2] && c[1][2] == c[2][2]) sum++;
-}
-
-void fun3() {
-    if(c[0][0] == c[1][1] && c[1][1] == c[2][2]) sum++;
-    if(c[0][2] == c[1][1] && c[1][1] == c[2][0]) sum++;
-}
-
-void fun4() {
-    set<char> s;
-    s.insert(c[0][0]);
-    s.insert(c[0][1]);
-    s.insert(c[0][2]);
-    if(s.size() == 2) sum2++;
-    set<char> s2;
-    s2.insert(c[1][0]);
-    s2.insert(c[1][1]);
-    s2.insert(c[1][2]);
-    if(s2.size() == 2) sum2++;
-    set<char> s3;
-    s3.insert(c[2][0]);
-    s3.insert(c[2][1]);
-    s3.insert(c[2][2]);
-    if(s3.size() == 2) sum2++;
-}
-
-void fun5() {
-    set<char> s;
-    s.insert(c[0][0]);
-    s.insert(c[1][0]);
-    s.insert(c[2][0]);
-    if(s.size() == 2) sum2++;
-    set<char> s2;
-    s2.insert(c[0][1]);
-    s2.insert(c[1][1]);
-    s2.insert(c[2][1]);
-    if(s2.size() == 2) sum2++;
-    set<char> s3;
-    s3.insert(c[0][2]);
-    s3.insert(c[1][2]);
-    s3.insert(c[2][2]);
-    if(s3.size() == 2) sum2++;
-}
-
-void fun6() {
-    set<int> s;
-    s.insert(c[0][0]);
-    s.insert(c[1][1]);
-    s.insert(c[2][2]);
-    if(s.size() == 2) sum2++;
-    set<int> s2;
-    s2.insert(c[0][2]);
-    s2.insert(c[1][1]);
-    s2.insert(c[2][0]);
-    if(s2.size() == 2) sum2++;
-}
-
-void Fun() {
-    fun1();
-    fun2();
-    fun3();
-    fun4();
-    fun5();
-    fun6();
-    cout << sum << endl << sum2;
-}
-int main() {
+int main()
+{
 #ifndef ONLINE_JUDGE
     freopen("1.in", "r", stdin);
 #endif
-    for(int i = 0; i < 3; i++)
-        for(int j = 0; j < 3; j++) cin >> c[i][j];
-    Fun();
+
+    for (int i = 0; i < 9; i++)
+        cin >> a[i];
+
+    fun();
+
+    int sum = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        if (s[i])
+            sum++;
+    }
+    cout << sum << endl;
+
+    sum = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        for (int j = 0; j < 26; j++)
+        {
+            if (d[i][j])
+                sum++;
+        }
+    }
+    cout << sum << endl;
+
     return 0;
 }
--- 2022暑假预科班/2/tempCodeRunnerFile.cpp
+++ 2022暑假预科班/2/tempCodeRunnerFile.cpp
@@ -4,29 +4,47 @@ using namespace std;
 int n;
 int a[100005], b[100005];
 
-void check(int i) {
-//    cout << a[i] << ' ' << b[i] << endl;
-    if (a[i] % 3 == 0 && a[i] == b[i]) {
+void check(int i)
+{
+    cout << a[i] << ' ' << b[i] << endl;
+    if (a[i] % 3 == 0 && a[i] == b[i])
+    {
         cout << "YES" << endl;
         return;
     }
-    if (a[i] * 2 == b[i] || b[i] * 2 == a[i]) {
+    if (a[i] * 2 == b[i] || b[i] * 2 == a[i])
+    {
         cout << "YES" << endl;
         return;
     }
-    if ((a[i] % 3 == 2 && a[i] % 3 == 1) || (a[i] % 3 == 1 && a[i] % 3 == 2)) {
+    if ((a[i] % 3 == 2 && b[i] % 3 == 1) || (a[i] % 3 == 1 && b[i] % 3 == 2))
+    {
         cout << "YES" << endl;
         return;
     }
     cout << "NO" << endl;
 }
 
-int main() {
+bool check2(int x, int y)
+{
+    if (x == 0 && y == 0)
+        return true;
+    if (x < 0 || y < 0)
+        return false;
+    return check2(x - 1, y - 2) || check2(x - 2, y - 1);
+}
+
+int main()
+{
+
 #ifndef ONLINE_JUDGE
     freopen("4.in", "r", stdin);
 #endif
+
+
     cin >> n;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> a[i] >> b[i];
     }
     /*
@@ -35,8 +53,10 @@ int main() {
     }
     */
 
-    for (int i = 0; i < n; i++) {
-        check(i);
+    for (int i = 0; i < n; i++)
+    {
+        cout << (check2(a[i], b[i]) ? "YES" : "NO") << endl;
+        
     }
     return 0;
 }
