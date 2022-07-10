70562ac8e7ca02be1ac9f49e65039ce2dee384f2
TangZichen0102 <110572306@qq.com>
2022-07-10 17:56:03 +0800

    save

--- 2022暑假预科班/5.cpp/4.cpp
+++ 2022暑假预科班/5.cpp/4.cpp
@@ -1,32 +1,18 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int a, b, c, d;
+int p[4][5] = {{461, 431, 420, 0},
+               {100, 57, 70, 0}, 
+               {130, 160, 118, 0}, 
+               {167, 266, 75, 0}
+};
+int sum = 0;
 int main() {
-  cin >> a >> b >> c >> d;
-  int sum = 0;
-  if(a == 1) sum += 461;
-  else if(a == 2) sum += 431;
-  else if(a == 3) sum += 420;
-  else sum += 0;
-
-
-  if(b == 1) sum += 130;
-  else if(b == 2) sum += 160;
-  else if(b == 3) sum += 118;
-  else sum += 0;
-
-
-  if(c == 1) sum += 100;
-  else if(c == 2) sum += 57;
-  else if(c == 3) sum += 70;
-  else sum += 0;
-
-
-  if(d == 1) sum += 167;
-  else if(d == 2) sum += 266;
-  else if(d == 3) sum += 75;
-  else sum += 0;
-  cout << sum;
-  return 0;
+    for(int i = 0; i < 4; i++) {
+        int _t;
+        cin >> _t;
+        sum += p[i][_t - 1];
+    }
+    cout << sum;
+    return 0;
 }
