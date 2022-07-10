ada36970ff6bcae455a29b477258791f8001e72d
TangZichen0102 <110572306@qq.com>
2022-07-10 16:43:05 +0800

    save

---
+++ 2022暑假预科班/5.cpp/4.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int a, b, c, d;
+int main() {
+  cin >> a >> b >> c >> d;
+  int sum = 0;
+  if(a == 1) sum += 461;
+  else if(a == 2) sum += 431;
+  else if(a == 3) sum += 420;
+  else sum = sum;
+  
+
+  if(b == 1) sum += 130;
+  else if(b == 2) sum += 160;
+  else if(b == 3) sum += 118;
+  else sum = sum;
+
+
+  if(c == 1) sum += 100;
+  else if(c == 2) sum += 57;
+  else if(c == 3) sum += 70;
+  else sum = sum;
+
+
+  if(d == 1) sum += 167;
+  else if(d == 2) sum += 266;
+  else if(d == 3) sum += 75;
+  else sum = sum;
+  cout << sum;
+  return 0;
+}
