d963832bb8cbf7b85b47d2c38d4280b09cc1d776
TangZichen0102 <110572306@qq.com>
2022-07-10 17:09:45 +0800

    save

--- 2022暑假预科班/5.cpp/4.cpp
+++ 2022暑假预科班/5.cpp/4.cpp
@@ -8,25 +8,25 @@ int main() {
   if(a == 1) sum += 461;
   else if(a == 2) sum += 431;
   else if(a == 3) sum += 420;
-  else sum = sum;
-  
+  else sum += 0;
+
 
   if(b == 1) sum += 130;
   else if(b == 2) sum += 160;
   else if(b == 3) sum += 118;
-  else sum = sum;
+  else sum += 0;
 
 
   if(c == 1) sum += 100;
   else if(c == 2) sum += 57;
   else if(c == 3) sum += 70;
-  else sum = sum;
+  else sum += 0;
 
 
   if(d == 1) sum += 167;
   else if(d == 2) sum += 266;
   else if(d == 3) sum += 75;
-  else sum = sum;
+  else sum += 0;
   cout << sum;
   return 0;
 }
