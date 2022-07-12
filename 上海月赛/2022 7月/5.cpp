55b9283f4ddeb4b70908584a9cf21035dee98e3d
TangZichen0102 <110572306@qq.com>
2022-07-12 15:53:22 +0800

    save

--- 上海月赛/2022 7月/5.cpp
+++ 上海月赛/2022 7月/5.cpp
@@ -1,106 +1,44 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+char a[1005][1005];
+char b[1005][1005];
 int n;
-void shang1() {
-    int cnt = n * 2;
-    int cnt2 = 1;
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = 0; j < cnt; j++) cout << " ";
-        if(i != 0) {
-            cout << "*";
-            for(int j = 0; j < cnt2; j++) cout << " ";
-            cnt2 += 2;
-            cout << "*";
-        }
-        else cout << "*";
-        cnt--;
-        cout << endl;
+void san1() {
+    int cnt1 = n * 3 - 3;
+    int cnt2 = 0;
+    for(int i = 1; i <= n * 3 - 3; i++) {
+        for(int j = 1; j <= cnt1; j++) a[i][j] = ' ';
+        a[i][cnt1 + 1] = '*';
+        for(int j = cnt1 + 2; j <= cnt1 + cnt2; j++) a[i][j] = ' ';
+        if(i != 1) a[i][cnt1 + cnt2 + 3] = '*'; 
+        cnt1--;
+        cnt2+= 2;
     }
-    for(int i = 0; i < 2 * n + 1; i++) cout << "* ";
-    cout << endl;
-}
-void shang2() {
-    int cnt = 1;
-    int cnt2 = 2 * n + 1 - 2 - ((n - 1) * 2 - 1) + 2;
-    // cout << cnt2;
-    int cnt3 = (n - 3) * 2 + 1;
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = 1; j <= cnt; j++) cout << " ";
-        cout << "*";
-        for(int j = 1; j <= cnt3; j++) cout << " ";
-        if(cnt3 != -1) cout << "*";
-        for(int j = 1; j <= cnt2 + 1; j++) cout << " ";
-        cout << "*";
-        for(int j = 1; j <= cnt3; j++) cout << " ";
-        if(cnt3 != -1) cout << "*";
-        cnt++;
-        cnt3 -= 2;
-        cnt2 += 2;
-        cout << endl;
+    cnt2 = n * 3 - 2;
+    for(int i = 1; i <= cnt2 * 2; i++) {
+        a[cnt2][i++] = '*';
+        a[cnt2][i] = ' ';
     }
-}
-void xia1() {
-    int cnt = n - 2;
-    int cnt2 = 1;
-    int cnt3 = 2 * n + 1 - 2 - ((n - 1) * 2 - 1) + 2 + (n - 2) * 2 - 1;
-    for(int i = 0; i < n - 2; i++) {
-        for(int j = 0; j < cnt; j++) cout << " ";
-        cout << "*";
-        for(int j = 0; j < cnt2; j++) cout << " ";
-        cout << "*";
-        for(int j = 0; j < cnt3; j++) cout << " ";
-        cout << "*";
-        for(int j = 0; j < cnt2; j++) cout << " ";
-        cout << "*";
-        cnt2 += 2;
-        cnt3 -= 2;
-        cnt--;
-        cout << endl;
-    }
-}
-void xia2() {
-    for(int i = 0; i < 2 * n + 1; i++) cout << "* ";
-    cout << endl;
-    int cnt = n * 2 - (n - 2);
-    int cnt2 = (n - 3) * 2 + 1;
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = 0; j < cnt; j++) cout << " ";
-        cout << '*';
-        for(int j = 0; j < cnt2; j++) cout << " ";
-        if(cnt2 != -1) cout << "*";
-        cnt++;
-        cnt2 -= 2;
-        cout << endl;
-    }
-}
-void shang() {
-    shang1(), shang2();
-}
-void xia() {
-    xia1(), xia2();
-}
-void res() {
-    shang(), xia();
 }
 int main() {
+    for(int i = 1; i <= n * 3 - 2 + (n - 1); i++)
+        for(int j = 1; j <= (n * 3 - 2) * 2; j++) a[i][j] = ' ', b[i][j] = ' ';
     cin >> n;
-    res();
+    san1();
+    // for(int i = 1; i <= n * 3 - 2 + (n - 1); i++) {
+    //     for(int j = 1; j <= (n * 3 - 2) * 2; j++) cout << a[i][j];
+    //     cout << endl;
+    // }
+    for(int i = n * 3 - 2 + (n - 1); i >= 1; i--) {
+        for(int j = 1; j <= (n * 3 - 2) * 2; j++) b[i][j] = a[i][j];
+    }
+    for(int i = n * 3 - 2 + (n - 1); i >= 1; i--) {
+        for(int j = 1; j <= (n * 3 - 2) * 2; j++) {
+            if(a[i][j] == '*' || b[i][j] == '*') cout << '*';
+            else cout << ' ';
+        }
+        cout << endl;
+    }
     return 0;
 }
-/*
-4
-        * 
-       * * 
-      *   * 
-* * * * * * * * * 
- *   *     *   * 
-  * *       * * 
-   *         * 
-  * *       * *
- * * *     * * *
-* * * * * * * * *
-      * * *
-       * *
-        *
-*/
