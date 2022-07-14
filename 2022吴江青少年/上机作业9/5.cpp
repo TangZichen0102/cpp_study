59676a5b379c746664621f7ee1bf20e2069d14bd
TT <tt-tjw@qq.com>
2022-07-14 14:51:15 +0800

    save

--- 2022吴江青少年/上机作业9/5.cpp
+++ 2022吴江青少年/上机作业9/5.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int pen() {
 	int sum = 0;
-	for(int i = 0; i <= 300; i++)
+	for(int i = 0; i <= 300; i+=3)
 		for(int j = 0; j <= 33; j++)
 			for(int k = 0; k <= 20; k++)
 				if(i + j + k == 100 && i / 3 + j * 3 + k * 5 == 100) sum++;
