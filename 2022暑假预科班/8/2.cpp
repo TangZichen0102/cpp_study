b4ae243162ebc94fd2470c54b4c5705b4eeba791
TangZichen0102 <110572306@qq.com>
2022-07-14 17:33:39 +0800

    save

--- 2022暑假预科班/8/2.cpp
+++ 2022暑假预科班/8/2.cpp
@@ -6,21 +6,19 @@ int minn = INT_MAX;
 int maxn = INT_MIN;
 int ans;
 int main() {
+#ifndef ONLINE_JUDGE
+    freopen("2.in", "r", stdin);
+#endif
     int n;
     cin >> n;
     for(int i = 0; i < n; i++) {
         int _t;
         cin >> _t;
         string s = to_string(_t);
-        // cout << s << endl;
         int sum = 1;
         for(int j = 0; j < s.size(); j++) sum *= s[j] - '0';
-        // cout << endl;
-        // cout << sum << endl;
         if(m[sum] == 0) m[sum]++, ans++;
-        else {
-            if(m[sum] >= maxn && sum <= minn) minn = sum, maxn = m[sum];
-        }
+        else if(m[sum] >= maxn && sum <= minn) minn = sum, maxn = m[sum];
     }
     cout << ans << " " << minn;
     return 0;
