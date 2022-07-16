bee10c55a686be3e16228ce3da907dc3a9f6107c
TangZichen0102 <110572306@qq.com>
2022-07-16 15:26:44 +0800

    save

--- 2022吴江青少年/上机作业11/Untitled7.cpp
+++ 2022吴江青少年/上机作业11/Untitled7.cpp
@@ -9,38 +9,38 @@ int main() {
 	i = j = n;
 	int t = n * n;
 	while(t >= 1) {
-		while(i == 0 && a[i][j] == 0) { // 上
+		while(i > 0 && a[i][j] == 0) { // 上
 			a[i][j] = t--;
 			i--;
-			cout << i << " " << j << endl;
+			// cout << i << " " << j << endl;
 		}
 		i++;
 		j--;
-		cout << endl;
-		while(j == 0 && a[i][j] == 0) { // 左
+		// cout << endl;
+		while(j > 0 && a[i][j] == 0) { // 左
 			a[i][j] = t--;
 			j--;
-			cout << i << " " << j << endl;
+			// cout << i << " " << j << endl;
 		}
 		j++;
 		i++;
-		cout << endl;
-		while(i == n && a[i][j] == 0) { // 下
+		// cout << endl;
+		while(i <= n && a[i][j] == 0) { // 下
 			a[i][j] = t--;
 			i++;
-			cout << i << " " << j << endl;
+			// cout << i << " " << j << endl;
 		}
 		i--;
 		j++;
-		cout << endl;
-		while(j == n && a[i][j] == 0) { // 右
+		// cout << endl;
+		while(j <= n && a[i][j] == 0) { // 右
 			a[i][j] = t--;
 			j++;
-			cout << i << " " << j << endl;
+			// cout << i << " " << j << endl;
 		}
 		j--;
 		i--;
-		cout << endl;
+		// cout << endl;
 	}
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < n; j++) cout << left << setw(5) << a[i][j];
