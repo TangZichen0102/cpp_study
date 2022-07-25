121e631b7f429e1ec690b12624972229369b4bce
TangZichen0102 <110572306@qq.com>
2022-07-25 13:58:40 +0800

    save

---
+++ PAT 乙级/1040.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int len = s.size(), result = 0, countp = 0, countt = 0;
+    for(int i = 0; i < len; i++) {
+        if(s[i] == 'T') countt++;
+    }
+    for(int i = 0; i < len; i++) {
+        if(s[i] == 'P') countp++;
+        if(s[i] == 'T') countt--;
+        if(s[i] == 'A') result = (result + (countp * countt) % 1000000007);
+    }
+    cout << result;
+    return 0;
+}
