6ba0eb55c6e865bf9eefbf45146d5b8e245fb248
TangZichen0102 <110572306@qq.com>
2022-08-01 12:20:21 +0800

    save

--- 2022吴江青少年/CSP强化班/2022-7-29A/作业第五题.cpp
+++ 2022吴江青少年/CSP强化班/2022-7-29A/作业第五题.cpp
@@ -3,51 +3,49 @@
 using namespace std;
 int a;
 string s;
-int main() {
-    scanf("%d.", &a);
+int tl(int x, int y) {
+    while (y--)
+        x *= 10;
+    return x;
+}
+int initNine(int n) {
+    int res = 0;
+    while (n--)
+        res = res * 10 + 9;
+    return res;
+}
+void fun() {
+    int a1 = 0, a2 = 0, a3 = 0, a, b;
+    string s, s2, s3;
+    scanf("%d.", &a1);
     cin >> s;
-    string _t;
-    if(s[0] == '(') {
-        int len = s.size() - 2;
-        for(int i = 0; i < len; i++) _t += '9';
+    int start = s.find("(");
+    if (~start) {
+        s2 = s.substr(0, start);
+        int end = s.find(")");
+        s3 = s.substr(start + 1, end - start - 1);
+        a2 = atoi(s2.c_str());
+        a3 = atoi(s3.c_str());
     }
-    if(s[0] != '(') {
-        int cnt = 0;
-        while(true) {
-            if(s[cnt] != '(') cnt++;
-            else break;
-        }
-        // cout << s[cnt];
-        int cnt2 = cnt;
-        while(true) {
-            if(s[cnt2] != ')') cnt2++;
-            else break;
-        }
-        for(int i = 0; i < cnt2 - cnt - 1; i++) _t += '9'; 
-        for(int i = 0; i < cnt; i++) _t += "0";
-        // cout << s[cnt2];
-        // string 
+    else a2 = atoi(s.c_str());
+    if (a3 > 0) {
+        a = tl(a2, s3.size()) + a3 - a2;
+        b = tl(initNine(s3.size()), s2.size()) - a2;
     }
-    // cout << _t;
-    string _t2;
-    if(a != 0) _t2 += to_string(a);
-    for(int i = 0; i < s.size(); i++)
-        if(s[i] >= '0' && s[i] <= '9') _t2 += s[i];
-    // cout << _t2;
-    string _t3;
-    if(a != 0) _t3 += to_string(a);
-    for(int i = 0; i < s.size(); i++)
-        if(s[i] >= '0' && s[i] <= '9') _t3 += s[i];
-        else break;
-    int sum1 = atoi(_t.c_str());
-    int sum2 = atoi(_t2.c_str());
-    int sum3 = atoi(_t3.c_str());
-    int sum4 = sum2 - sum3;
-    // cout << sum1 << " " << sum4;
-    // cout <<  __gcd(sum4, sum1);
-    int _T = sum1;
-    sum1 = sum1 / __gcd(sum4, sum1);
-    sum4 = sum4 / (_T / sum1);
-    cout << sum4 << '/' << sum1;
+    else
+    {
+        a = a2;
+        b = tl(1, s2.size() + 1);
+    }
+    int g = __gcd(a, b);
+    cout << a / g << "/" << b / g << endl;
+}
+int main() {
+    freopen("5.in", "r", stdin);
+    int n;
+    cin >> n;
+    while(n--) {
+        fun();
+    }
     return 0;
 }
