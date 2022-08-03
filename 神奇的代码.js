f3ece757fb2d0c6431cbc03919675d3b2ad4b28c
TT <tt-tjw@qq.com>
2022-08-03 15:56:18 +0800

    save

---
+++ 神奇的代码.js
@@ -0,0 +1,7 @@
+var code = document.getElementById("code");
+code.oncopy = () => true;
+code.oncut = () => true;
+code.onselectstart = () => true;
+code.oncontextmenu = () => true;
+code.id = "code2";
+scrollX
