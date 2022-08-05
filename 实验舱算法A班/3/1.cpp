e9e05bb103e7259e1195c66d4c30144a135a70c4
TangZichen0102 <110572306@qq.com>
2022-08-05 10:20:13 +0800

    save

--- 实验舱算法A班/3/1.cpp
+++ 实验舱算法A班/3/1.cpp
@@ -1,13 +1,20 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int n, c, r;
+long long _t1, _t2;
+long long sum = 0;
 int main() {
+    long long n;
     cin >> n;
-    int sum = 0;
-    for(int i = 0; i < n; i++) {
-        cin >> c >> r;
-        sum += c + r;
+    for(long long i = 0; i < n; i++) {
+        long long r, c;
+        cin >> r >> c;
+        // cout << sum << " " << _t1 << " " << _t2 << endl;
+        _t1 = max(r, c) - min(r, c);
+        if(i == 0) sum += r + c;
+        else sum += r + c - _t2;
+        // cout << sum << " " << _t1 << " " << _t2 << endl;
+        _t2 = _t1 - _t2;
     }
     cout << sum;
     return 0;
--- 实验舱算法A班/3/4.cpp
+++ 实验舱算法A班/3/4.cpp
@@ -2,28 +2,28 @@
 
 using namespace std;
 char c[7005][7005];
-int n, m, k;
+long long n, m, k;
 int main() {
     cin >> n >> m >> k;
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < m; j++) c[i][j] = 'B';
-    for(int i = 0; i < k; i++) {
+    for(long long i = 0; i < n; i++)
+        for(long long j = 0; j < m; j++) c[i][j] = 'B';
+    for(long long i = 0; i < k; i++) {
         char d;
-        int a;
+        long long a;
         cin >> d >> a;
         a = a - 1;
         if(d == 'R')
-            for(int j = 0; j < m; j++)
+            for(long long j = 0; j < m; j++)
                 if(c[a][j] == 'B') c[a][j] = 'G';
                 else c[a][j] = 'B';
         else
-            for(int j = 0; j < n; j++)
+            for(long long j = 0; j < n; j++)
                 if(c[j][a] == 'B') c[j][a] = 'G';
                 else c[j][a] = 'B';
     }
-    int sum = 0;
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < m; j++)
+    long long sum = 0;
+    for(long long i = 0; i < n; i++)
+        for(long long j = 0; j < m; j++)
             if(c[i][j] == 'G') sum++;
     cout << sum;
     return 0;
