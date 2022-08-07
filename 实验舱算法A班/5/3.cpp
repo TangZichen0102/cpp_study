1903afcf083d0e70b536e8eea0beab52b06f9acb
TangZichen0102 <110572306@qq.com>
2022-08-07 12:20:46 +0800

    save

---
+++ 实验舱算法A班/5/3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+    }
+    int p, l;
+    cin >> p >> l;
+    cout << -1;
+    return 0;
+}
