3b202d8ffd3f2bcd62f83c782b1a8b4a92b51d18
TangZichen0102 <110572306@qq.com>
2022-08-07 22:26:58 +0800

    save

--- 实验舱作业/5/7充电器.cpp
+++ 实验舱作业/5/7充电器.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+int sum;
 int main() {
     int n, m;
     cin >> n >> m;
-    int sum = 0;
     while(n > 0 && m > 0) {
         if(n > m) m++, n -= 2;
         else n++, m -= 2;
