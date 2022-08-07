a0524f60567a0b3e01f9c4dcf859bfbaca702b97
TangZichen0102 <110572306@qq.com>
2022-08-07 18:32:11 +0800

    save

---
+++ 实验舱作业/5/2加勒比海盗.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n, c;
+int a[10005];
+int sum = 0, cnt = 0;
+int main() {
+    cin >> c >> n;
+    for(int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a + n);
+    for(int i = 0; i < n; i++) {
+        cnt += a[i];
+        if(cnt > )
+    }
+    return 0;
+}
