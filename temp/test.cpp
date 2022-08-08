760191ca0c9979ffe5f74595ea660b43828f1090
TangZichen0102 <110572306@qq.com>
2022-08-08 16:27:58 +0800

    save

--- temp/test.cpp
+++ temp/test.cpp
@@ -8,7 +8,14 @@ typedef long long LL;
 LL n, m, sum;
 int main()
 {
-   cin >> n;
+   vector<int> v(3);
+
+   v[0] = 1, v[1] = 2, v[2] = 3;
+   v.erase(v.end() - 1);
+   for (auto i : v)
+   {
+      cout << i << " ";
+   }
    return 0;
 }
 /*
