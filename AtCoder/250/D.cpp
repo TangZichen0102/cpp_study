73e6ff45884129cb3dc62304fc48919725d65c66
TangZichen0102 <110572306@qq.com>
2022-08-09 09:10:34 +0800

    save

--- AtCoder/250/D.cpp
+++ AtCoder/250/D.cpp
@@ -2,29 +2,39 @@
 
 using namespace std;
 const int MAXN = 9e6 + 5;
-using u64 = long long;
-u64 n;
+using LL = long long;
+LL n;
 bool st[MAXN];
-u64 primes[MAXN], cnt;
-void get_primes(int n) {
+LL primes[MAXN], cnt;
+void get_primes(int n)
+{
 	st[1] = 0;
-	for (u64 i = 2; i <= n; i++) {
-		if (st[i] == 0) continue;
+	for (LL i = 2; i <= n; i++) 
+	{
+		if (st[i] == 0)
+			continue;
 		primes[cnt++] = i;
-		for (int j = i + i; j <= n; j += i)	st[j] = 0;
+		for (int j = i + i; j <= n; j += i)
+			st[j] = 0;
 	}
 }
-u64 s[MAXN];
-int main() {
-	//freopen("D.in", "r", stdin);
-	u64 ans = 0;
+LL s[MAXN];
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("D.in", "r", stdin);
+#endif
+	LL ans = 0;
 	memset(st, 1, sizeof(st));
 	get_primes(MAXN);
-	for(u64 i = 1; i <= MAXN; i++) s[i] = s[i - 1] + st[i];
+	for (LL i = 1; i <= MAXN; i++)
+		s[i] = s[i - 1] + st[i];
 	cin >> n;
-	for(u64 i = 1; i < cnt; i++) {
-		u64 x = primes[i] * primes[i] * primes[i];
-		if(x >= n) break;
+	for (LL i = 1; i < cnt; i++)
+	{
+		LL x = primes[i] * primes[i] * primes[i];
+		if (x >= n)
+			break;
 		ans += s[min(n / x, primes[i] - 1)];
 	}
 	cout << ans;
