36a8a70bb207f82772d51087959759951012f0b0
TT <tt-tjw@qq.com>
2022-08-10 20:56:22 +0800

    save

--- 实验舱/实验舱算法A班/比赛/7/2.cpp
+++ 实验舱/实验舱算法A班/比赛/7/2.cpp
@@ -5,29 +5,37 @@ typedef long long LL;
 const int N = 1e7 + 10;
 int primes[N], euler[N], cnt;
 bool st[N];
-void get_euler(int n) {
-    for(int i = 2; i <= n; i++) {
-        if(!st[i]) {
+void get_eulers(int n)
+{
+    euler[1] = 1; //约定1的欧拉函数是
+    for (int i = 2; i <= n; i++)
+    {
+        if (!st[i])
+        {
             primes[cnt++] = i;
             euler[i] = i - 1;
         }
-        for(int j = 0; primes[j] <= n / i; j++) {
+        for (int j = 0; primes[j] <= n / i; j++)
+        {
             int t = primes[j] * i;
             st[t] = true;
-            if (i % primes[j] == 0) {
+            if (i % primes[j] == 0)
+            {
                 euler[t] = euler[i] * primes[j];
                 break;
             }
             euler[t] = euler[i] * (primes[j] - 1);
-        } 
+        }
     }
 }
-int main() {
+int main()
+{
     int n;
     cin >> n;
     get_eulers(n);
     LL res = 0;
-    for (int i = 2; i <= n; i++) res += euler[i];
+    for (int i = 2; i <= n; i++)
+        res += euler[i];
     cout << res << endl;
     return 0;
 }
