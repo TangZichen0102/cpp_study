f9ef5b2762454b5ebef16c699be905679007f358
TT <tt-tjw@qq.com>
2022-08-11 21:33:29 +0800

    save

--- temp/test.cpp
+++ temp/test.cpp
@@ -4,42 +4,25 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
+
+#define IOS ios::sync_with_stdio(false), cin.tie(0)
+#define _for(i, a, b) for (int i = (a); i < (b); ++i)
+#define _rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define br puts("")
+
 typedef long long LL;
-int n, a;
-
-template <class T>
-void read(T &r)
-{
-   r = 0;
-   char c = getchar();
-   bool f = false;
-   while (c < 48 || c > 57)
-   {
-      if (c == 45)
-         f ^= 1;
-      c = getchar();
-   }
-   while (c >= 48 && c <= 57)
-   {
-      r = 10 * r + c - 48;
-      c = getchar();
-   }
-   if (f)
-      r = -r;
-}
-
-#include <bits/stdc++.h>
-using namespace std;
+int n;
 int main()
 {
-   scanf("%d", &n);
-   scanf("%d", &a);
-   while (--n)
-   {
-      int b;
-      read(b);
-      printf("%d ", b);
-   }
-   printf("%d", a);
+   IOS;
+
+   _for(i, 0, 4) cout << i << endl;
+   br;
+   _rep(j, 0, 4) cout << j << endl;
+
    return 0;
 }
+/*
+样例及推导
+
+*/
