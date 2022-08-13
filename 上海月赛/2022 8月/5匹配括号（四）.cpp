7c43a55b98b8f61a326269d2d712c2fec5ae345f
TT <tt-tjw@qq.com>
2022-08-13 14:17:53 +0800

    save

--- 上海月赛/2022 8月/5匹配括号（四）.cpp
+++ 上海月赛/2022 8月/5匹配括号（四）.cpp
@@ -6,21 +6,15 @@
 using namespace std;
 
 #define IOS ios::sync_with_stdio(false), cin.tie(0)
-#define _for(i, a, b) for (int i = (a); i < (b); ++i)
-#define _rep(i, a, b) for (int i = (a); i <= (b); ++i)
-#define br puts("")
-
 typedef long long LL;
-const int mod = 1e9 + 7;
 int n;
-typedef pair<int, int> PII;
-PII exp(string s, int deep)
+double exp(string s, int deep)
 {
     cout << "exp:" << s << endl;
     int sum = 0;
-    if (s == "")
+    if (s.empty())
     {
-        return {1, 1};
+        return 0.5;
     }
     int cnt1 = 0, cnt2 = 0, l = 0, r = 0;
     for (int i = 0; i < s.size(); i++)
@@ -40,13 +34,12 @@ PII exp(string s, int deep)
 
         if (cnt1 == cnt2)
         {
-            printf("cnt1:%d cnt2:%d l:%d r:%d deep:%d\n", cnt1, cnt2, l, r, deep);
             cnt1 = cnt2 = 0;
-            auto ans = exp(s.substr(l + 1, r - l - 1), deep + 1);
-            sum += exp(s.substr(l + 1, r - l - 1), deep + 1);
+            auto ans = exp(s.substr(l + 1, r - l - 1), deep + 1) * 2.0;
+
+            sum += ans;
         }
     }
-
     return sum;
 }
 
@@ -56,10 +49,8 @@ int main()
 #ifndef ONLINE_JUDGE
     freopen("5.in", "r", stdin);
 #endif
-
     string s;
     cin >> s;
-
     cout << exp(s, 0);
     return 0;
 }
