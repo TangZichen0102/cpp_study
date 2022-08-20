1cf6c330eeb187bd81289a731d84d2a382e29c28
TangZichen0102 <110572306@qq.com>
2022-08-20 15:51:29 +0800

    save

--- 实验舱/实验舱算法A班/比赛/15/4.cpp
+++ 实验舱/实验舱算法A班/比赛/15/4.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int n, a[1000010];
-int idx, cnt;
+int n, a[1000100], idx, cnt;
 int main() {
     cin >> n;
     for(int i = 0; i < n; i++) cin >> a[i];
     while(idx < n) {
         cnt++;
         int d = a[idx];
-        if(idx == 0) idx += d;
-        else if(a[idx + d] >= max(a[idx - d], 0)) idx += d;
+        if(idx == 0 || idx - d < 0) idx += d;
+        else if(a[min(idx + d, n)] >= a[max(idx - d, 0)]) idx += d;
         else idx = max(idx - d, 0);
+        printf("cnt:%d\nidx:%d,a[idx]:%d\n", cnt, idx, a[idx]);
     }
     cout << cnt;
     return 0;
