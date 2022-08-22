51a54fe919a6adcc20f41db4dba4cbf5b1e11325
TT <tt-tjw@qq.com>
2022-08-22 11:38:12 +0800

    save

--- temp/next.cpp
+++ temp/next.cpp
@@ -1,53 +1,20 @@
-/*
-题目来源
+#include<bits/stdc++.h>
 
-*/
-#include <bits/stdc++.h>
 using namespace std;
-typedef long long LL;
-int n;
-
-unordered_map<string, int> dp;
-
 int main()
 {
-
-    LL sum = 1;
-    for (int i = 0; i <= 62; i++)
-    {
-        dp[to_string(sum)] = 1;
-        sum *= 2;
-    }
-
-    // for (auto i : dp)
-    // {
-    //     cout << i.first << endl;
-    // }
-
-    string n = "223372036854775807";
-
-    vector<int> v;
-    for (int i = 0; i < n.size(); i++)
-    {
-        v.push_back(i);
-    }
-
-    int cnt = 0;
-    do
-    {
-        string s;
-        for (auto i : v)
-        {
-            s += n[i];
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        unsigned int a,cnt=0;
+        int b;
+        cin>>a>>b;
+	    if(a % abs(b) != 0 || b == 0) {
+	    	cout << "NO";
+            return 0;
         }
-        if (dp[s])
-            cout << s << " YES" << endl;
-        cnt++;
-    } while (next_permutation(v.begin(), v.end()));
-    // cout << cnt;
+        while(a!=0)a+=b;
+        cout<<"YES";
+    }
     return 0;
 }
-/*
-样例及推导
-
-*/
---
+++ 吴江青少年科技馆/2022CSP强化班/CSP强化班/2022-8-22A/3 最长公共子序列/1 最长公共子序列的长度.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+void Output(vector<vector<int> > &M)
+{
+	for(int i=0; i<M.size(); i++)	
+	{
+		for(int j=0; j<M[i].size(); j++)	
+			cout<<setw(5)<<M[i][j];
+		cout<<endl;
+	}
+}
+/*  C[i][j] = max(C[i-1][j],  C[i][j-1] )
+                           i
+  v1: X  X  X  X  X  X  X  X 
+  v2: X  X  X  X  X  X  X  X  X  X 
+                                 j
+*/
+int LCS(vector<int> &v1, vector<int> &v2)
+{
+	int m=v1.size(),n=v2.size();
+	vector<vector<int> > C(m+1, vector<int>(n+1,0));  // ±È½Ï¾ØÕó
+	for(int i=1; i<=m; i++)
+		for(int j=1; j<=n; j++)
+		{
+			if(v1[i-1]==v2[j-1])
+				C[i][j]=C[i-1][j-1]+1;
+			else
+				if(C[i][j-1] > C[i-1][j])
+					C[i][j]=C[i][j-1];
+				else 
+					C[i][j]=C[i-1][j];
+		}
+	Output(C);
+	return C[m][n];
+}
+/*
+	v2[]=   ¿Õ {2, 4, 3, 1, 2, 1};
+	    ¿Õ   0  0  0  0  0  0  0
+	v1[]={1, 0  0  0  0  1  1  1
+		  2, 0  1  1  1  1  2  2  
+		  3, 0  1  1  2  2  2  2
+		  2, 0  1  1  2  2  3  3
+		  4, 0  1  2  2  2  3  3
+		  1, 0  1  2  2  3  3  4 
+		  2: 0  1  2  2  3  4  4
+*/
+int main()
+{
+	int x1[]={1,2,3,2,4,1,2},n1=7; 
+	int x2[]={2,4,3,1,2,1}  ,n2=6;   
+	vector<int> v1(x1,x1+n1),  v2(x2,x2+n2);
+	int max=LCS(v1,v2);
+	cout<<max<<endl;
+}
+
+/*
+	v2[]=   ¿Õ {2, 4, 3, 1, 2, 1};
+	    ¿Õ   0  0  0  0  0  0  0
+	v1[]={1, 0  0  0  0  1  1  1
+		  2, 0  1  1  1  1  2  2
+		  3, 0  1  1  2  2  2  2
+		  2, 0  1  1  2  2  3  3
+		  4, 0  1  2  2  2  3  3  
+		  1, 0  1  2  2  3  3  4
+		  2: 0  1  2  2  3  4  4
+*/
+
