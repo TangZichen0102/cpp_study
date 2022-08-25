c7917cc64daa66bd35ada9541089a2e4a2cc3041
TangZichen0102 <110572306@qq.com>
2022-08-25 15:38:53 +0800

    save

---
+++ 实验舱/实验舱算法A班/比赛/13/4.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int d[6][3] = {{1, 0, 0}, {-1, 0, 0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}};
+int n, m, l, t, ans;
+bool maze[33][129][65], vis[33][129][65];
+struct node {
+    int x, y, z;
+} cur;
+void bfs(int x, int y, int z) {
+    queue<>
+}
+int main() {
+
+    return 0;
+}
