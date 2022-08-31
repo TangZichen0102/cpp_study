e9f36dd21addbd4dffa16b54015d1d3f90ad0d2a
TangZichen0102 <110572306@qq.com>
2022-08-31 13:32:05 +0800

    save

--- temp/next.cpp
+++ temp/next.cpp
@@ -1,8 +1,17 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+unsigned long long a[5100], S[5100];
 int main() {
-
+    int t;
+    cin >> t;
+    a[1] = 1, a[2] = 1;
+    for(int i = 3; i <= 4000; i++) a[i] = a[i - 1] + a[i - 2];
+    for(int i = 1; i <= 4000; i++) S[i] = S[i - 1] + a[i];
+    for(int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        cout << S[n] << endl;
+    }
     return 0;
 }
--- temp/tempCodeRunnerFile.cpp
+++ temp/tempCodeRunnerFile.cpp
@@ -1,24 +1,5 @@
-#include <bits/stdc++.h>
 
-using namespace std;
-int cnt;
-int main() {
-    int n;
-    cin >> n;
-    string a;
-    while(cin >> a) {
-        if(a == "926718") {
-            cout << "yes";
-            return 0;
-        }
-        else {
-            cnt++;
-            if(cnt == n) {
-                cout << "lock";
-                return 0;
-            }
-            cout << "no" << endl;
-        }
-    }
-    return 0;
-}
+    //     int n;
+    //     cin >> n;
+    //     cout << S[n] << endl;
+    // }
