a10535fa3264ffabcd7d42ddf8b7a7a3db0657ef
TangZichen0102 <110572306@qq.com>
2022-09-12 16:03:23 +0800

    save

--- next2.cpp
+++ next2.cpp
@@ -1,15 +1,27 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int a[1005][1005];
+int d[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+bool is_leap_year(int n) {
+    if((n % 4 == 0 && n % 100 != 0) || n % 400 == 0) return true;
+    return false;
+}
+int days(int n) {
+    int sum = 0;
+    for(int i = 1600; i <= n - 1; i++) sum += (is_leap_year(i) ? 366 : 365);
+    for(int i = 1; i <= 5; i++) sum += d[i];
+    sum += 27;
+    if(is_leap_year(n)) sum++;
+    return sum;
+}
 int main() {
-    int n, m;
-    cin >> n >> m;
-    for(int i = 1; i <= n; i++) 
-        for(int j = 1; j <= m; j++) cin >> a[i][j];
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= m; j++) cout << a[i][j] << " ";
-        cout << endl;
+    int ans = 0;
+    int a, b;
+    cin >> a >> b;
+    for(int i = a; i <= b; i++) {
+        int sum = (days(i) + 6) % 8;
+        if(sum == 6 || sum == 7) ans++;
     }
+    cout << ans;
     return 0;
 }
