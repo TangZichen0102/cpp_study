35a6bd2a59d5d27f0b29e7fef834cad511c9fad5
TT <tt-tjw@qq.com>
2022-09-21 22:47:43 +0800

    save

--- temp/3.cpp
+++ temp/3.cpp
@@ -1,7 +1,3 @@
-/*
-题目来源
-
-*/
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long LL;
@@ -9,72 +5,33 @@ typedef long long LL;
 string dp1 = "0123456789abcdefghijklmnopqrstuvwxyz";
 
 map<char, int> dp2 = {
-    {'0', 0},
-    {'1', 1},
-    {'2', 2},
-    {'3', 3},
-    {'4', 4},
-    {'5', 5},
-    {'6', 6},
-    {'7', 7},
-    {'8', 8},
-    {'9', 9},
-    {'A', 10},
-    {'B', 11},
-    {'C', 12},
-    {'D', 13},
-    {'E', 14},
-    {'F', 15},
-    {'G', 16},
-    {'H', 17},
-    {'I', 18},
-    {'J', 19},
-    {'K', 20},
-    {'L', 21},
-    {'M', 22},
-    {'N', 23},
-    {'O', 24},
-    {'P', 25},
-    {'Q', 26},
-    {'R', 27},
-    {'S', 28},
-    {'T', 29},
-    {'U', 30},
-    {'V', 31},
-    {'W', 32},
-    {'X', 33},
-    {'Y', 34},
-    {'Z', 35},
-
-};
-
-int qmi(int a, int k)
+    {'0', 0}, {'1', 1}, {'2', 2}, {'3', 3}, {'4', 4}, {'5', 5}, {'6', 6}, {'7', 7}, {'8', 8}, {'9', 9}, {'A', 10}, {'B', 11}, {'C', 12}, {'D', 13}, {'E', 14}, {'F', 15}, {'G', 16}, {'H', 17}, {'I', 18}, {'J', 19}, {'K', 20}, {'L', 21}, {'M', 22}, {'N', 23}, {'O', 24}, {'P', 25}, {'Q', 26}, {'R', 27}, {'S', 28}, {'T', 29}, {'U', 30}, {'V', 31}, {'W', 32}, {'X', 33}, {'Y', 34}, {'Z', 35}};
+long long qmi(long long a, int k)
 {
-    int res = 1;
+    long long res = 1;
     while (k)
     {
-        if (k & 1)
-            res = (LL)res * a;
-        k >>= 1;
-        a = (LL)a * a;
+        if (k & 1) res = (LL)res * a;
+        k >>= 1, a = (LL)a * a;
     }
     return res;
 }
-string _Itoa(int n, int m)
+string _Itoa(long long n, int m)
 {
     string _s;
-    do
+    while (n)
     {
         int t = n % m;
-        if (t >= 0 && t <= 9)
-            _s += t + '0';
-        else
-            _s += t - 10 + 'a';
+        _s += dp1[t];
         n /= m;
-    } while (n != 0);
+    }
     reverse(_s.begin(), _s.end());
     return _s;
 }
+
+int M, N;
+string n;
+
 int main()
 {
     ios::sync_with_stdio(0);
@@ -82,27 +39,19 @@ int main()
     // #ifndef ONLINE_JUDGE
     //     freopen("3.in", "r", stdin);
     // #endif
-    int M, N;
-    cin >> M >> N;
-    string n;
-    while (cin >> n)
+    while (cin >> M >> N >> n)
     {
         int l = n.size();
-
-        long long ans = 0;
-
+        unsigned long long ans = 0;
         for (int i = l - 1, j = 0; i >= 0; i--, j++)
         {
             // cout << n[i];
             ans += dp2[n[i]] * qmi(M, j);
             // cout << ans << endl;
         }
+        // cout << ans << endl;
         cout << _Itoa(ans, N) << endl;
     }
 
     return 0;
 }
-/*
-样例及推导
-
-*/
---
+++ 实验舱/实验舱题库/2826.cpp
@@ -0,0 +1,54 @@
+/*
+题目来源
+https://oj.shiyancang.cn/Problem/2826.html
+#2826、修剪草地
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+int n, t, ans = 9999999, cnt = 0;
+
+int vis[2010][2010];
+
+map<char, vector<int>> d = {
+    {'E', {0, 1}},  // 东
+    {'W', {0, -1}}, //西
+    {'S', {1, 0}},  //南
+    {'N', {-1, 0}}  // 北
+};
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("2826.in", "r", stdin);
+#endif
+    cin >> n;
+    int x = 1000, y = 1000;
+    while (n--)
+    {
+        char D;
+        int s;
+        cin >> D >> s;
+        for (int i = 1; i <= s; i++)
+        {
+            t++;
+            x += d[D][0], y += d[D][1];
+
+            if (vis[x][y] != 0)
+            {
+                cnt++;
+                // cout << n << " " << D << " " << s << endl;
+                // cout << x << " " << y << endl;
+                // cout << t << " " << vis[x][y] << endl;
+                // cout << t << " " << vis[x][y] << " " << ans;
+                ans = min(t - vis[x][y], ans);
+                // cout << endl;
+            }
+            vis[x][y] = t;
+
+        }
+    }
+    cout << (cnt ? ans : -1);
+    return 0;
+}
