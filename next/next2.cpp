987f53ea169b637e132fbbc7bf34992d7f94254d
TangZichen0102 <110572306@qq.com>
2022-10-13 21:53:12 +0800

    save

--- next/next2.cpp
+++ next/next2.cpp
@@ -1,11 +1,21 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int n, m;
+int n;
+double m;
 struct node {
-    int score, id;
+    int id, score;
 } a[5010];
+bool cmp(node x, node y) {
+    if(x.score != y.score) x.score > y.score;
+    return x.id < y.id;
+}
 int main() {
-    
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++) cin >> a[i].id >> a[i].score;
+    int d = floor(m * 1.5);
+    sort(a + 1, a + n + 1, cmp);
+    cout << a[d].score << " " << d << endl;
+    for(int i = 1; i <= d; i++) cout << a[i].id << " " << a[i].score << endl;
     return 0;
 }
