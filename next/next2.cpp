addf3780e224ec525851cf3caac6da02450c7698
TangZichen0102 <110572306@qq.com>
2022-12-07 21:45:42 +0800

    save

--- next/next2.cpp
+++ next/next2.cpp
@@ -1,35 +1,20 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int a = 1, b = 2;
-int n, flag, cnt = 1;
+long long x, y, ans;
+long long gcd(long long a, long long b) {
+    return b ? gcd(b, a % b) : a;
+}
 int main() {
-// #ifndef ONLINE_JUDGE
-//     freopen("2.in", "r", stdin);
-// #endif
-    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-    cin >> n;
-    if(n == 1) {
-        cout << "1/1";
+    cin >> x >> y;
+    if(x == y) {
+        cout << 1;
         return 0;
     }
-    for(int i = 2; i <= n - 1; i++) {
-        if(i != 2 && cnt == 1) {
-            if(a == 1) {
-                b++, flag = 0;
-                cnt = 0;
-                continue;
-            }
-            if(b == 1) {
-                a++, flag = 1;
-                cnt = 0;
-                continue;
-            }
-        }
-        cnt = 1;
-        if(flag == 0) a++, b--;
-        if(flag == 1) a--, b++;
+    long long sum = x * y;
+    for(int i = 3; i <= sum / i; i++) {
+        if(sum % i == 0 && gcd(i, sum / i) == x) ans++;
     }
-    cout << a << "/" << b;
+    cout << ans * 2;
     return 0;
 }
