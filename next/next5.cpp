bdc495faab0286a85000ff2e66cf586d8a842d4d
TangZichen0102 <110572306@qq.com>
2022-12-10 21:01:44 +0800

    save

--- next/next5.cpp
+++ next/next5.cpp
@@ -1,18 +1,8 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-const long long mod = 1000000007;
-int n;
-long long sum = 1, ans;
-long long a[105];
+
 int main() {
-    cin >> n;
-    for(int i = 0; i < n; i++) {
-        cin >> a[i];
-        sum = sum * a[i] % mod;
-    }
-    for(int i = 1; i <= sum / i; i++)
-        if(sum % i == 0) ans = ans + i + sum / i;
-    cout << ans;
+    
     return 0;
 }
