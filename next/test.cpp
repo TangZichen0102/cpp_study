d090f64ba32e8ddf19e060778f77611ce575f00f
TangZichen0102 <110572306@qq.com>
2023-01-09 17:24:16 +0800

    save

--- next/test.cpp
+++ next/test.cpp
@@ -1,31 +1,12 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
-int n, _t;
-int x, sum;
-multiset<int> s;
+int n, k, tot, pr[1000010];
 int main() {
-    cin >> n;
-    for(int i = 1; i <= n; i++) {
-        cin >> _t;
-        s.insert(_t);
+    cin >> n >> k;
+    for(int i = 1; i < n; ++i) {
+        if(__gcd(i, n) == 1) pr[++tot] = i;
     }
-    cin >> x;
-    int flag = 0;
-    for(auto i: s)
-        if(i == x) {
-            flag = 1;
-            break;
-        }
-    if(flag == 0) s.insert(x), sum++;
-    int idx = 0;
-    for(auto i: s) {
-        idx++;
-        if(i == x) break;
-    }
-    // cout << s.size() - idx << " " << idx - 1 << endl;
-    int d1 = s.size() - idx, d2 = idx - 1;
-    sum += abs(d1 - d2);
-    cout << sum;
+    cout << (k - 1) / tot * n + pr[k % tot];
     return 0;
 }
