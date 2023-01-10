edadb78118ffa836de893a729826153e58c9ac39
TangZichen0102 <110572306@qq.com>
2023-01-10 17:01:25 +0800

    save

--- next/test.cpp
+++ next/test.cpp
@@ -1,12 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
-int n, k, tot, pr[1000010];
 int main() {
-    cin >> n >> k;
-    for(int i = 1; i < n; ++i) {
-        if(__gcd(i, n) == 1) pr[++tot] = i;
-    }
-    cout << (k - 1) / tot * n + pr[k % tot];
+    int n;
+    cin >> n;
+    stringstream s;
+    s.precision(0);
+    s << fixed << pow(2.0L, n + 1);
+    string a = s.str();
+    a[a.length() - 1] -= 2;
+    cout << a << endl;
     return 0;
 }
