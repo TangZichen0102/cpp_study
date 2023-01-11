2b2528a470a33d915d28181a8424043d14fc5ed9
TangZichen0102 <110572306@qq.com>
2023-01-11 19:51:16 +0800

    save

--- next/test.cpp
+++ next/test.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+int l, i, f;
+char s[201], k[201];
 int main() {
-    int n;
-    cin >> n;
-    stringstream s;
-    s.precision(0);
-    s << fixed << pow(2.0L, n + 1);
-    string a = s.str();
-    a[a.length() - 1] -= 2;
-    cout << a << endl;
+    scanf("%s", s);
+    l = strlen(s) - 1;
+    while (s[i++] == '9')
+        if (i == l + 1)
+            for (s[0] = '1', l++; i > 0; i--) s[i] = '0';
+    for (i = 0; i <= l - i; i++) k[i] = k[l - i] = s[i];
+    if (strcmp(k, s) <= 0) {
+        while (k[--i] == '9') ;
+        k[i] = k[l - i] = ++k[i];
+        for (i++; i <= l - i; i++) k[i] = k[l - i] = '0';
+    }
+    cout << k;
     return 0;
 }
