9145e99b80f8c6431d814b9a3fdca6ad1bcf6bc3
TangZichen0102 <110572306@qq.com>
2023-01-15 20:15:30 +0800

    save

--- test2.cpp
+++ test2.cpp
@@ -1,19 +1,51 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int a[105][105];
-int dp[105][105];
-int main() {
-    int n;
-    cin >> n;
-    while(n--) {
-        int r, c;
-        cin >> r >> c;
-        for(int i = 1; i <= r; i++)
-            for(int j = 1; j <= c; j++) cin >> a[i][j];
-        for(int i = 1; i <= r; i++)
-            for(int j = 1; j <= c; j++) dp[i][j] = max(dp[i -1][j], dp[i][j - 1]) + a[i][j];
-        cout << dp[r][c] << endl;
+
+int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = (x << 1) + (x << 3) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+int t, n, s[1005], ans, p1, p2;
+int main()
+{
+    t = read();
+    while (t--)
+    {
+        n = read();
+        for (int i = 1; i <= n; i++)
+            s[i] = read();
+        sort(s + 1, s + n + 1);
+        ans = 0;
+        while (n > 0)
+        {
+            if (n == 1)
+                ans += s[1];
+            else if (n == 2)
+                ans += s[2];
+            else if (n == 3)
+            {
+                ans += s[1] + s[2] + s[3];
+                break;
+            }
+            else
+            {
+                p1 = s[n] + s[n - 1] + 2 * s[1];
+                p2 = s[2] * 2 + s[1] + s[n];
+                ans += min(p1, p2);
+            }
+            n -= 2;
+        }
+        printf("%d\n", ans);
     }
     return 0;
 }
