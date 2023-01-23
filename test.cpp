d05286c80014310167111667adb682d5326df82f
TangZichen0102 <110572306@qq.com>
2023-01-23 21:18:08 +0800

    save

--- test.cpp
+++ test.cpp
@@ -1,25 +1,11 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-priority_queue<int, vector<int>, greater<int>> q;
+int sum;
+int n;
 int main() {
-    int n;
-    scanf("%d", &n);
-    int t;
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &t);
-        q.push(t);
-    }
-    int t1, t2, sum, temp = 0;
-    while (q.size() != 1) {
-        t1 = q.top();
-        q.pop();
-        t2 = q.top();
-        q.pop();
-        sum = t1 + t2;
-        temp += sum;
-        q.push(sum);
-    }
-    printf("%d", temp);
+    cin >> n;
+    for(int i = 1; i <= n; i++) sum += n / i;
+    cout << sum;
     return 0;
 }
